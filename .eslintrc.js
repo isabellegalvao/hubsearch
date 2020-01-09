08b2f70a2750fc011ee4383a7b09211d9dfeff4f
Vinicius Ribeiro <contato@ribeirovinicius.com>
2020-01-09 01:51:01 -0300

    :rotating_light: ignored history from prop-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,6 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'react/state-in-constructor': 'off',
     'react/jsx-props-no-spreading': 0,
+    'react/prop-types': [2, { ignore: ['children', 'history'] }],
   },
 };
