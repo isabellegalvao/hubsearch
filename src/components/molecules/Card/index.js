e48aa8858ca26577bb9a4d5c7657bfce06708b39
Vinicius Ribeiro <contato@ribeirovinicius.com>
2020-01-09 13:18:58 -0300

    :dizzy: added animation to card component

--- src/components/molecules/Card/index.js
+++ src/components/molecules/Card/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 import Title from '../../atoms/Title';
 import Tagline from '../../atoms/Tagline';
@@ -9,16 +10,29 @@ import Topic from '../../atoms/Topic';
 import { CardWrapper, CardLink } from './style';
 
 const Card = ({ className, owner, stars, title, tech }) => {
+  const variants = {
+    hidden: { opacity: 0, y: '100px' },
+    visible: { opacity: 1, y: '0' },
+  };
+
   return (
     <>
-      <CardWrapper className={className}>
-        <Topic item={stars} text="Stars" />
-        <Title text={title} />
-        <Tagline text={tech} />
-        <CardLink>
-          <Link to={{ pathname: `/${owner}/${title}` }}>SEE DETAILS</Link>
-        </CardLink>
-      </CardWrapper>
+      <motion.div
+        className={className}
+        initial="hidden"
+        animate="visible"
+        variants={variants}
+        transition={{ duration: 0.5 }}
+      >
+        <CardWrapper>
+          <Topic item={stars} text="Stars" />
+          <Title text={title} />
+          <Tagline text={tech} />
+          <CardLink>
+            <Link to={{ pathname: `/${owner}/${title}` }}>SEE DETAILS</Link>
+          </CardLink>
+        </CardWrapper>
+      </motion.div>
     </>
   );
 };
