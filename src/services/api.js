f9dea1fe79b0f3c332ca7befa8d6dfa0165ed7a3
Vinicius Ribeiro <contato@ribeirovinicius.com>
2020-01-09 03:42:48 -0300

    :wrench: added github token

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,7 @@ const api = axios.create({
   baseURL: 'https://api.github.com',
 });
 
+api.defaults.headers.common['Authorization'] =
+  process.env.REACT_APP_NOT_SECRET_CODE;
+
 export default api;
